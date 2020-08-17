1e1ae7c83f5177f55daf3a116a6b9b4fac5b943d
Marcon Willian <marconwillian@protonmail.com>
2020-08-17 07:41:47 -0300

    fix: Update verify User

--- src/utils/verifyUserAuthenticate.ts
+++ src/utils/verifyUserAuthenticate.ts
@@ -1,14 +1,6 @@
-import { Response } from 'express';
-import { RequestAuth } from '../middlewares/auth'
+import { SocketAuth } from '../middlewares/auth'
 
-export const authorizationUserIsAdmin = (request: RequestAuth, response: Response) => {
-	if(!verifyUserIsAdmin(request))
-		return response.status(403).json({statusCode: 403, error: "Forbidden", message: "access denied"});
-
-	return true;
-};
-
-const verifyUserIsAdmin = (request: RequestAuth) => {
-	let TypeUserAuthenticate =  request._userAuthenticate?.type;
+export const verifyUserIsAdmin = (socket: SocketAuth) => {
+	let TypeUserAuthenticate =  socket.decode?.type;
 	return TypeUserAuthenticate=='1';
 };
