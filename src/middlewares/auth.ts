e58f91dbde286fd11518787f27d9cd3f44dfde61
Marcon Willian <marconwillian@protonmail.com>
2020-08-28 23:20:14 -0300

    Update base auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,46 +1,59 @@
-import { Socket } from "socket.io";
+import { Socket } from 'socket.io';
 
-
-
-import jwt, {DecodeOptions} from 'jsonwebtoken';
-
+import jwt, { DecodeOptions } from 'jsonwebtoken';
 
 interface UserAuthenticate extends DecodeOptions {
-    type: String;
+  type: string;
 }
 
 export interface VerifySocker {
-    status: boolean;
-    decode: {
-        type: String;
-    };
-    messageError: any;
+  status: boolean;
+  decode: {
+    type: string;
+  };
+  messageError: any;
 }
 
 export interface SocketAuth extends Socket {
-    decode?: {
-        type: String;
-    }
+  decode?: {
+    type: string;
+  };
 }
 
 export const verifySocker = (socket: SocketAuth, next: Function) => {
-    try {
-        if(!socket.handshake.query)
-            throw {statusCode: 401, error: "Unauthorized", message: "No token provided"}
+  try {
+    if (!socket.handshake.query)
+      throw {
+        statusCode: 401,
+        error: 'Unauthorized',
+        message: 'No token provided'
+      };
 
-        let token = socket.handshake.query.token;
+    const { token } = socket.handshake.query;
 
-        if(!token)
-            throw {statusCode: 401, error: "Unauthorized", message: "No token provided"}
+    if (!token)
+      throw {
+        statusCode: 401,
+        error: 'Unauthorized',
+        message: 'No token provided'
+      };
 
-        jwt.verify(token, process.env.HASH_1_SECRET as string, (err: any, decoded: any) => {
-            if(err)
-                throw {statusCode: 401, error: "Unauthorized", message: "Token invalid"}
+    jwt.verify(
+      token,
+      process.env.HASH_1_SECRET as string,
+      (err: any, decoded: any) => {
+        if (err)
+          throw {
+            statusCode: 401,
+            error: 'Unauthorized',
+            message: 'Token invalid'
+          };
 
-            socket.decode = decoded as UserAuthenticate;
-            next()
-        })
-    } catch (err) {
-        console.log(err);
-    }
-}
+        socket.decode = decoded as UserAuthenticate;
+        next();
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
